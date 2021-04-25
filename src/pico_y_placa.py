8815295e6279683d900e0f86b2872ffb4cf0243d
Mayra Macas Carrasco <macas.carrasco@protonmail.com>
2021-04-25 01:51:22 -0500

    Update moving holidays (According to law 858)

--- src/pico_y_placa.py
+++ src/pico_y_placa.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 import datetime
 import requests
 import os
@@ -9,23 +10,15 @@ from dateutil.relativedelta import relativedelta as rd, FR
 from holidays.constants import JAN, MAY, AUG, OCT, NOV, DEC
 from holidays.holiday_base import HolidayBase
 
-
 class HolidayEcuador(HolidayBase):
     """
     A class to represent a Holiday in Ecuador by province (HolidayEcuador)
     It aims to make determining whether a 
     specific date is a holiday as fast and flexible as possible.
-    It inherits the HolidayBase class of holidays.
     https://www.turismo.gob.ec/wp-content/uploads/2020/03/CALENDARIO-DE-FERIADOS.pdf
-    -Document related to lent
-    https://es.wikipedia.org/wiki/Cuaresma    
-    -Regularation about rules that should be aplied when national 
-    and / or local holidays coincide in continuous days
-    https://www.ccq.ec/wp-content/uploads/2017/06/Consulta-Laboral-Diciembre-2016.pdf
-    https://www.trabajo.gob.ec/wp-content/uploads/downloads/2012/11/C%C3%B3digo-de-Tabajo-PDF.pdf
     ...
 
-    Attributes
+    Attributes (It inherits the HolidayBase class)
     ----------
     prov: str
         province code according to ISO3166-2
@@ -35,69 +28,136 @@ class HolidayEcuador(HolidayBase):
     __init__(self, plate, date, time, online=False):
         Constructs all the necessary attributes for the HolidayEcuador object.
     _populate(self, year):
-        Returns dates that are holidays in a given year
+        Returns if a date is holiday or not
     """     
     # ISO 3166-2 codes for the principal subdivisions, called provinces
     # https://es.wikipedia.org/wiki/ISO_3166-2:EC
     PROVINCES = ["EC-P"]  # TODO add more provinces
 
     def __init__(self, **kwargs):
-        self.country = "EC"
+        """
+        Constructs all the necessary attributes for the HolidayEcuador object.
+        """         
+        self.country = "ECU"
         self.prov = kwargs.pop("prov", "ON")
         HolidayBase.__init__(self, **kwargs)
 
     def _populate(self, year):
-        # New Year's Day
-        self[datetime.date(year, JAN, 1)] = "Año Nuevo [New Year's Day]"
+        """
+        Checks if a date is holiday or not
+        
+        Parameters
+        ----------
+        year : str
+            year of a date
+
+        Returns
+        -------
+        Returns true if a date is a holiday otherwise flase 
+        """                    
+        # New Year's Day 
+        self[date(year, JAN, 1)] = "Año Nuevo [New Year's Day]"
+        
+        # Christmas
+        self[date(year, DEC, 25)] = "Navidad [Christmas]"
         
         # Holy Week
-        name_fri = "Semana Santa (Viernes Santo) [Good Friday)]"
-        name_easter = "Día de Pascuas [Easter Day]"
-        self[easter(year) + rd(weekday=FR(-1))] = name_fri
-        self[easter(year)] = name_easter
-
+        self[easter(year) + rd(weekday=FR(-1))] = "Semana Santa (Viernes Santo) [Good Friday)]"
+        self[easter(year)] = "Día de Pascuas [Easter Day]"
+        
         # Carnival
         total_lent_days = 46
-        name_mondaycarnival = "Lunes de carnaval [Carnival of Monday)]"
-        name_tuesdaycarnival = "Martes de carnaval [Tuesday of Carnival)]"
-        self[easter(year) - datetime.timedelta(days=total_lent_days + 2)
-             ] = name_mondaycarnival
-        self[easter(year) - datetime.timedelta(days=total_lent_days + 1)
-             ] = name_tuesdaycarnival
-
-        # Labor Day
+        self[easter(year) - timedelta(days=total_lent_days+2)] = "Lunes de carnaval [Carnival of Monday)]"
+        self[easter(year) - timedelta(days=total_lent_days+1)] = "Martes de carnaval [Tuesday of Carnival)]"
+        
+        # Labor day
         name = "Día Nacional del Trabajo [Labour Day]"
-        self[datetime.date(year, MAY, 1)] = name
-
-        # Pichincha battle
+        # (Law 858) If the holiday falls on Saturday or Tuesday
+        # the mandatory rest will go to the immediate previous Friday or Monday
+        # respectively
+        if year > 2015 and date(year, MAY, 1).weekday() in (5,1):
+            self[date(year, MAY, 1) - timedelta(days=1)] = name
+        # (Law 858) if the holiday falls on Sunday
+        # the mandatory rest will go to the following Monday
+        elif year > 2015 and date(year, MAY, 1).weekday() == 6:
+            self[date(year, MAY, 1) + timedelta(days=1)] = name
+        # (Law 858) Holidays that are on Wednesday or Thursday
+        # will be moved to the Friday of that week
+        elif year > 2015 and  date(year, MAY, 1).weekday() in (2,3):
+            self[date(year, MAY, 1) + rd(weekday=FR)] = name
+        else:
+            self[date(year, MAY, 1)] = name
+        
+        # Pichincha battle, the rules are the same as the labor day
         name = "Batalla del Pichincha [Pichincha Battle]"
-        self[datetime.date(year, MAY, 31)] = name
-
-        # First Cry of Independence
+        if year > 2015 and date(year, MAY, 24).weekday() in (5,1):
+            self[date(year, MAY, 24).weekday() - timedelta(days=1)] = name
+        elif year > 2015 and date(year, MAY, 24).weekday() == 6:
+            self[date(year, MAY, 24) + timedelta(days=1)] = name
+        elif year > 2015 and  date(year, MAY, 24).weekday() in (2,3):
+            self[date(year, MAY, 24) + rd(weekday=FR)] = name
+        else:
+            self[date(year, MAY, 24)] = name        
+        
+        # First Cry of Independence, the rules are the same as the labor day
         name = "Primer Grito de la Independencia [First Cry of Independence]"
-        self[datetime.date(year, AUG, 10)] = name
-
-        # Guayaquil's independence
+        if year > 2015 and date(year, AUG, 10).weekday() in (5,1):
+            self[date(year, AUG, 10)- timedelta(days=1)] = name
+        elif year > 2015 and date(year, AUG, 10).weekday() == 6:
+            self[date(year, AUG, 10) + timedelta(days=1)] = name
+        elif year > 2015 and  date(year, AUG, 10).weekday() in (2,3):
+            self[date(year, AUG, 10) + rd(weekday=FR)] = name
+        else:
+            self[date(year, AUG, 10)] = name       
+        
+        # Guayaquil's independence, the rules are the same as the labor day
         name = "Independencia de Guayaquil [Guayaquil's Independence]"
-        self[datetime.date(year, OCT, 9)] = name
-
-        # Day of the Dead
-        name = "Día de los difuntos [Day of the Dead]"
-        self[datetime.date(year, NOV, 2)] = name
-
+        if year > 2015 and date(year, OCT, 9).weekday() in (5,1):
+            self[date(year, OCT, 9) - timedelta(days=1)] = name
+        elif year > 2015 and date(year, OCT, 9).weekday() == 6:
+            self[date(year, OCT, 9) + timedelta(days=1)] = name
+        elif year > 2015 and  date(year, MAY, 1).weekday() in (2,3):
+            self[date(year, OCT, 9) + rd(weekday=FR)] = name
+        else:
+            self[date(year, OCT, 9)] = name        
+        
+        # Day of the Dead and
+        namedd = "Día de los difuntos [Day of the Dead]" 
         # Independence of Cuenca
-        name = "Independencia de Cuenca [Independence of Cuenca]"
-        self[datetime.date(year, NOV, 3)] = name
-
-        # Foundation of Quito
-        if self.prov == "EC-P":
-            name = "Fundación de Quito [Foundation of Quito]"
-            self[datetime.date(year, DEC, 6)] = name
-
-        # Christmas
-        name = "Navidad [Christmas]"
-        self[datetime.date(year, DEC, 25)] = name
-
+        nameic = "Independencia de Cuenca [Independence of Cuenca]"
+        #(Law 858) National and/or local holidays coincide on continuous days, 
+        #the following rules will apply:
+        if (date(year, NOV, 2).weekday() == 5 and  date(year, NOV, 3).weekday() == 6):
+            self[date(year, NOV, 2) - timedelta(days=1)] = namedd
+            self[date(year, NOV, 3) + timedelta(days=1)] = nameic     
+        elif (date(year, NOV, 3).weekday() == 2):
+            self[date(year, NOV, 2)] = namedd
+            self[date(year, NOV, 3) - timedelta(days=2)] = nameic
+        elif (date(year, NOV, 3).weekday() == 3):
+            self[date(year, NOV, 3)] = nameic
+            self[date(year, NOV, 2) + timedelta(days=2)] = namedd
+        elif (date(year, NOV, 3).weekday() == 5):
+            self[date(year, NOV, 2)] =  namedd
+            self[date(year, NOV, 3) - timedelta(days=2)] = nameic
+        elif (date(year, NOV, 3).weekday() == 0):
+            self[date(year, NOV, 3)] = nameic
+            self[date(year, NOV, 2) + timedelta(days=2)] = namedd
+        else:
+            self[date(year, NOV, 2)] = namedd
+            self[date(year, NOV, 3)] = nameic  
+            
+        # Foundation of Quito, applies only to Pichincha province, 
+        # the rules are the same as the labor day
+        name = "Fundación de Quito [Foundation of Quito]"        
+        if self.prov in ("EC-P"):
+            if year > 2015 and date(year, DEC, 6).weekday() in (5,1):
+                self[date(year, DEC, 6) - timedelta(days=1)] = name
+            elif year > 2015 and date(year, DEC, 6).weekday() == 6:
+                self[(date(year, DEC, 6).weekday()) + timedelta(days=1)] =name
+            elif year > 2015 and  date(year, DEC, 6).weekday() in (2,3):
+                self[date(year, DEC, 6) + rd(weekday=FR)] = name
+            else:
+                self[date(year, DEC, 6)] = name
 
 class PicoPlaca:
     """
